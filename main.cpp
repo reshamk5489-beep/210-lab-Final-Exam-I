6ae6f8280f3c43c4c2bb062429f1b6b1d10e6840
Resham <resha@MacBook-Air-2.local>
2025-12-10 18:46:27 -0800

    Calculated highest traffic count

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
     map<string, int> airportFlightsCount;
     string originAirport;
     string destinationAirport;
+    int maxFlightsCount;
 
     while (file >> originAirport >> destinationAirport) 
     {
@@ -21,10 +22,25 @@ int main()
     cout << "Airport Flight Counts:" << endl;
     for (auto &p : airportFlightsCount)
     {
+        maxFlightsCount = 0;
         cout << "\t" << p.first << ": " << p.second << endl;
+
+        if (p.second >= maxFlightsCount) 
+        {
+            maxFlightsCount = p.second;
+        }
+    }
+
+    cout << "Max Flight Count: " << maxFlightsCount << endl;
+
+    cout << "Busiest Airport Counts:" << endl;
+    for (auto &p : airportFlightsCount)
+    {
+        if (p.second == maxFlightsCount)
+        {
+            cout << "\t" << p.first << ": " << p.second << endl;
+        }
     }
 
     return 0;
 }
-
-// Milestone 1
